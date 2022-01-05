5568504d7256acc0f9f78f9c001d950a85dadec8
Cindy Wänlund <cindywanlund@Cindys-MBP.lan>
2022-01-05 18:32:58 +0100

    lagt in http request

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
@@ -6,26 +7,33 @@ import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Tasty Schnitzel', 
-            'A super-tasty Schnitzel', 
-            'https://images.unsplash.com/photo-1524859330668-c357331384f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe('Big Fat Burger', 
-        'What else you need to say?', 
-        'https://images.unsplash.com/photo-1524859330668-c357331384f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80',
-        [
-            new Ingredient('Buns', 2),
-            new Ingredient('Meat', 1)
-        ])
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Tasty Schnitzel', 
+    //         'A super-tasty Schnitzel', 
+    //         'https://images.unsplash.com/photo-1524859330668-c357331384f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]),
+    //     new Recipe('Big Fat Burger', 
+    //     'What else you need to say?', 
+    //     'https://images.unsplash.com/photo-1524859330668-c357331384f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2076&q=80',
+    //     [
+    //         new Ingredient('Buns', 2),
+    //         new Ingredient('Meat', 1)
+    //     ])
+    //   ];
+    private recipes: Recipe[] = [];
 
       constructor(private slService: ShoppingListService) {}
+
+      setRecipes(recipes: Recipe[]) {
+          this.recipes = recipes;
+          this.recipesChanged.next(this.recipes.slice());
+      }
 
       getRecipes() {
           return this.recipes.slice();
@@ -37,5 +45,20 @@ export class RecipeService {
 
       addIngredientsToShoppingList(ingredients: Ingredient[]) {
           this.slService.addIngredients(ingredients);
+      }
+
+      addRecipe(recipe: Recipe) {
+          this.recipes.push(recipe);
+          this.recipesChanged.next(this.recipes.slice());
+      }
+
+      updateRecipe(index: number, newRecipe: Recipe) {
+          this.recipes[index] = newRecipe;
+          this.recipesChanged.next(this.recipes.slice());
+      }
+
+      deleteRecipe(index: number) {
+          this.recipes.splice(index, 1);
+          this.recipesChanged.next(this.recipes.slice());
       }
 }
